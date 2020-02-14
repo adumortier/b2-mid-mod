58ed52c99edbd00e48cd9dc8b3585031b8002837
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-14 10:22:52 -0700

    Edit the movie spec

--- spec/models/movies_spec.rb
+++ spec/models/movies_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe Movie, type: :model do
 
   describe 'relationships' do
     it {should belong_to :studio}
+    it {should have_many(:actor_movies)}
+    it {should have_many(:actors).through(:actor_movies)}
   end
 
   # describe 'methods' do 
